1a22fad813ea49ea2416fe4e8a196bb4bd3dc235
MaomiHz <maomihz@gmail.com>
2015-04-12 09:59:14 +0800

    modift RectFish

--- src/com/seancheey/data/RectFish.java
+++ src/com/seancheey/data/RectFish.java
@@ -1,8 +1,7 @@
 package com.seancheey.data;
 
-
 import java.awt.Graphics;
-
+import java.awt.Color;
 import com.seancheey.Main;
 
 public class RectFish extends Fish {
@@ -11,27 +10,40 @@ public class RectFish extends Fish {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public RectFish(int width, int height, double x, double y, double vx,
-			double vy, Pond pond) {
-		super(width, height, x, y, vx, vy, pond);
+	
+	private Color color;
+	
+	// Constructor list
+	public RectFish(int width, int height, double x, double y, double vx, double vy, Pond pond) {
+		this(width, height, x, y, vx, vy, pond, Main.randColor()); //Give a random color to fish
 	}
-
+	
+	public RectFish(int width, int height, double x, double y, double vx, double vy, Pond pond, Color c) {
+		super(width, height, x, y, vx, vy, pond);
+		color = c;
+	}
+	
+	// Draw itself
 	@Override
 	protected void paint(Graphics g) {
+		g.setColor(color);
 		g.fillRect((int) (x - width / 2), (int) (y - height / 2), width, height);
 	}
-
+	
+	
+	// ***Main Algorithm is here!!!***
 	@Override
 	protected void move() {
-		//Touch the wall to reflect
+		// Touch the wall to reflect
 		if (x > getPond().getWidth() || x < 0) {
 			vx *= -0.92;
 		}
 		if (y > getPond().getHeight() || y < 0) {
 			vy *= -0.92;
 		}
-		//to prevent the ball from sticking into wall
+		
+		// Prevent the ball from sticking into wall
+		// Haha, asean!
 		if (x > getPond().getWidth()) {
 			x = Main.WIDTH-width;
 		}
@@ -44,12 +56,13 @@ public class RectFish extends Fish {
 		if (y < 0) {
 			y = height;
 		}
-		//have a move
+		
+		// Make a move
 		x += vx;
 		y += vy;
-		//pick a random fish
+		
+		// Pick a random fish & close to it. 
 		Fish randFish=pond.getFishes().get(Math.abs((int)x%pond.getFishes().size()));
-		//close to it
 		vx+=(randFish.getX()-x)/1000;
 		vy+=(randFish.getY()-y)/1000;
 	}
